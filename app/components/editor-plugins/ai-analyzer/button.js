310fb53f2ee0a779b4810b5f35d6652f875ac0bf
abeforgit <arnebertrand@gmail.com>
2024-10-01 15:42:18 +0200

    feat: basic annotation thing

--- app/components/editor-plugins/ai-analyzer/button.js
+++ app/components/editor-plugins/ai-analyzer/button.js
@@ -1,9 +1,82 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { task, timeout } from 'ember-concurrency';
 
+const responses = [
+  { uri: 'http://data.vlaanderen/ABB', foundIn: 'ABB' },
+  {
+    uri: 'http://data.vlaanderen/ABB',
+    foundIn: 'Agentschap Binnenlands Bestuur',
+  },
+  { uri: 'http://data.vlaanderen/Test', foundIn: 'Test', alwaysFind: true },
+];
+
+/**
+ * @typedef {object} Args
+ * @property {import('@lblod/ember-rdfa-editor').SayController?} controller
+ * */
+
+/**
+ * @extends {Component<Args>}
+ * */
 export default class EditorPluginsAiAnalyzerButtonComponent extends Component {
   @tracked modalOpen = false;
-  openModal = () => {
+  @tracked spots = [];
+
+  get controller() {
+    return this.args.controller;
+  }
+  get content() {
+    return this.controller.htmlContent;
+  }
+
+  findTextInEditor = () => {
+    const foundSpots = [];
+    const doc = this.controller?.mainEditorState.doc;
+    const unfoundResponses = new Set();
+    for (const response of responses) {
+      doc.descendants((node, pos) => {
+        if (node.isText) {
+          /** @type {string} */
+          const content = node.textContent;
+          const matches = [...content.matchAll(response.foundIn)];
+          if (
+            !matches.length &&
+            response.alwaysFind &&
+            !unfoundResponses.has(response.uri)
+          ) {
+            foundSpots.push({ type: response.uri, found: false });
+            unfoundResponses.add(response.uri);
+          }
+          for (const match of matches) {
+            console.log('match', match);
+            const index = match.index;
+            const textPos = pos + index;
+            const foundText = match[0];
+            const len = foundText.length;
+            const contextText = foundText;
+            foundSpots.push({
+              from: textPos,
+              to: textPos + len,
+              text: foundText,
+              contextText,
+              type: response.uri,
+              found: true,
+            });
+          }
+        }
+      });
+    }
+    return foundSpots;
+  };
+  fetchAiResponsesTask = task(async () => {
+    // await timeout(400);
+    // console.log('FETCH', this.content);
+  });
+
+  openModal = async () => {
+    this.spots = this.findTextInEditor();
+    await this.fetchAiResponsesTask.perform();
     this.modalOpen = true;
   };
 
--- app/components/editor-plugins/ai-analyzer/modal.js
+++ app/components/editor-plugins/ai-analyzer/modal.js
@@ -1,3 +1,36 @@
 import Component from '@glimmer/component';
+/**
+ * @typedef {object} Args
+ * @property {import('@lblod/ember-rdfa-editor').SayController?} controller
+ * */
 
-export default class EditorPluginsAiAnalyzerModalComponent extends Component {}
+/**
+ * @extends {Component<Args>}
+ * */
+export default class EditorPluginsAiAnalyzerModalComponent extends Component {
+  get controller() {
+    return this.args.controller;
+  }
+  get schema() {
+    return this.controller?.schema;
+  }
+
+  annotate = (spot) => {
+    this.controller.withTransaction(
+      (t) => {
+        t.replaceRangeWith(
+          spot.from,
+          spot.to,
+          this.schema.node('inline_rdfa', {}, this.schema.text(spot.text)),
+        );
+
+        return t;
+      },
+      { view: this.controller.mainEditorView },
+    );
+    this.args.closeModal();
+  };
+  docAnnotate = (spot) => {
+    this.args.closeModal();
+  };
+}
